3f825bbe26397f6fb78f9794cabe0f4b06101690
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-03-29 11:31:47 +0530

    add new

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const db = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://mitulbhimani281:mF6u0wongMtNZE3l@cluster0.t7dse.mongodb.net/NovuscoreDrones").then((res) => {
-    console.log('DB is Conected');
-})
-.catch((err) => {
+// MongoDB Connection
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://mitulbhimani281:mF6u0wongMtNZE3l@cluster0.t7dse.mongodb.net/NovuscoreDrones'; // Replace with your MongoDB URI
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('DB is Connected');
+    })
+    .catch((err) => {
         console.log('Error connecting to the database:', err);
-   });
+    });
 
 // Middleware
-app.use(cors())
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
