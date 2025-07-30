0253fd7ccb7ba12547fbaff66d539f8e0ba190dc
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-07-30 19:28:50 +0530

    push

--- portfolio/api.js
+++ portfolio/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const submitContactForm = async (formData) => {
     try {
-        const response = await axios.post('https://novuscore-drones.vercel.app/api/contact', formData, {
+        const response = await axios.post('https://novuscore-drone.vercel.app/api/contact', formData, {
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -14,4 +14,4 @@ export const submitContactForm = async (formData) => {
     }
 };
 // http://localhost:3000/api/contact
-// vercel https://novuscore-drones.vercel.app/api/contact
+// vercel https://novuscore-drone.vercel.app/api/contact
